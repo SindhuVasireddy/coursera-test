8690b8244315d9563dcd4a0ae36d3600b431e25d
SindhuVasireddy <77059383+SindhuVasireddy@users.noreply.github.com>
2021-10-31 16:37:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+// Event handling
 document.addEventListener("DOMContentLoaded",
-	function(event){
-		//Unobstrusive Event Binding
-		document.querySelector("button").addEventListener("click",
-			function(){
-				var self=this;
-				var name="";
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
 
-				//Call the server to get a name. Remember!!!This is an asynchronous call. It is not going to wait till the server responds to execute the next line which in our case is to print to console the output of the server.
-				$ajaxUtils.sendGetRequest("name.txt",
-					function (request){
-						self.name=request.responseText;//responseText Property of the request object is what holds the response to our name query.
-						console.log(self.name)
-						document.querySelector("#content").innerHTML="<h2>Hello"+self.name+"!</h2>";
-					}
-					)
-			})
-				// document.querySelector("#content").innerHTML="<h2>Hello"+self.name+"!";//We cannot have this line outside the asynchronous call since this line will be executed even before a response is got back from the server!!!
-			}
-			)
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
+
+        
+      });
+  }
+);
